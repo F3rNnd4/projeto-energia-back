609b2c20fe51ffe6fee5325dab69b548a6b500bf
Fernanda Alves <fernandaalves.tds2.senai@gmail.com>
2025-05-15 15:19:38 -0300

    feat: add comodo model and controller

---
+++ src/model/comodo.model.js
@@ -0,0 +1,58 @@
+import prisma from "../../prisma/prisma.js";
+
+class ComodoModel {
+  // Método para obter todos os cômodos
+  async findAll() {
+    const comodos = await prisma.comodo.findMany({
+      orderBy: {
+        id: "asc",
+      },
+      include: {
+        dispositivos: {
+          select: {
+            nome: true,
+            potencia: true,
+            tempoUso: true,
+            voltagem: true,
+            corrente: true,
+          },
+        },
+      },
+    });
+
+    const totalComodos = comodos.length;
+
+    return {
+      comodos,
+      totalComodos,
+    };
+  }
+
+  // Método para obter um cômodo específico pelo ID
+    async findById(id) {
+        const comodo = await prisma.comodo.findUnique({
+            where: {
+                id: Number(id),
+            },
+            include: {
+                dispositivos: true,
+            },
+        });
+
+        return comodo;
+    }
+
+    // Método para criar um novo cômodo
+    async create(name) {
+        const novoComodo = await prisma.comodo.create({
+            data: {
+                nome
+            },
+        });
+
+        return novoComodo;
+    }
+
+}
+
+export default new ComodoModel;
