8b581671e0c6c937e71bd92f653f9507339ba6a9
Sophia Balico <sophiabalico.tds2.senai@gmail.com>
2025-05-23 14:52:50 -0300

    feat: add new dispositivos to the seed

--- prisma/seed/seed.js
+++ prisma/seed/seed.js
@@ -60,6 +60,50 @@ async function main() {
         }
     });
 
+    const tv = await prisma.dispositivo.create({
+        data: {
+            nome: "TV",
+            potencia: 100,
+            tempoUso: 4,
+            voltagem: 110,
+            corrente: 0.91,
+            comodoId: sala.id
+        }
+    });
+
+    const homeTheater = await prisma.dispositivo.create({
+        data: {
+            nome: "Home Theater",
+            potencia: 250,
+            tempoUso: 3,
+            voltagem: 110,
+            corrente: 2.27,
+            comodoId: sala.id
+        }
+    });
+
+    const roteador = await prisma.dispositivo.create({
+        data: {
+            nome: "Roteador Wi-fi",
+            potencia: 20,
+            tempoUso: 24,
+            voltagem: 110,
+            corrente: 0.09,
+            comodoId: sala.id
+        }
+    });
+
+    const abajur = await prisma.dispositivo.create({
+        data: {
+            nome: "Abajur",
+            potencia: 40,
+            tempoUso: 6,
+            voltagem: 110,
+            corrente: 0.36,
+            comodoId: sala.id
+        }
+    });
+
     const geladeira = await prisma.dispositivo.create({
         data: {
             nome: "Geladeira",
@@ -67,6 +111,61 @@ async function main() {
             tempoUso: 24,
             voltagem: 220,
             corrente: 0.68,
+            comodoId: cozinha.id
+        }
+    });
+
+    const microondas = await prisma.dispositivo.create({
+        data: {
+            nome: "Microondas",
+            potencia: 1200,
+            tempoUso: 1,
+            voltagem: 220,
+            corrente: 5.45,
+            comodoId: cozinha.id
+        }
+    });
+
+    const airfryer = await prisma.dispositivo.create({
+        data: {
+            nome: "Air Fryer",
+            potencia: 1500,
+            tempoUso: 2,
+            voltagem: 220,
+            corrente: 6.82,
+            comodoId: cozinha.id
+        }
+    });
+
+    const cafeteira = await prisma.dispositivo.create({
+        data: {
+            nome: "Cafeteira",
+            potencia: 600,
+            tempoUso: 1,
+            voltagem: 110,
+            corrente: 5.45,
+            comodoId: cozinha.id
+        }
+    });
+
+    const liquidificador = await prisma.dispositivo.create({
+        data: {
+            nome: "Liquidificador",
+            potencia: 500,
+            tempoUso: 0.5,
+            voltagem: 110,
+            corrente: 4.55,
+            comodoId: cozinha.id
+        }
+    });
+
+    const exaustor = await prisma.dispositivo.create({
+        data: {
+            nome: "Exaustor",
+            potencia: 100,
+            tempoUso: 2,
+            voltagem: 220,
+            corrente: 0.45,
             comodoId: cozinha.id
         }
     });
@@ -82,50 +181,6 @@ async function main() {
         }
     });
 
-    const ventilador = await prisma.dispositivo.create({
-        data: {
-            nome: "Ventilador",
-            potencia: 80,
-            tempoUso: 6,
-            voltagem: 110,
-            corrente: 0.73,
-            comodoId: quarto.id
-        }
-    });
-
-    const tv = await prisma.dispositivo.create({
-        data: {
-            nome: "TV",
-            potencia: 100,
-            tempoUso: 4,
-            voltagem: 110,
-            corrente: 0.91,
-            comodoId: sala.id
-        }
-    });
-
-    const microondas = await prisma.dispositivo.create({
-        data: {
-            nome: "Microondas",
-            potencia: 1200,
-            tempoUso: 1,
-            voltagem: 220,
-            corrente: 5.45,
-            comodoId: cozinha.id
-        }
-    });
-
-    const computador = await prisma.dispositivo.create({
-        data: {
-            nome: "Computador",
-            potencia: 300,
-            tempoUso: 8,
-            voltagem: 110,
-            corrente: 2.73,
-            comodoId: quarto.id
-        }
-    });
-
     const secador = await prisma.dispositivo.create({
         data: {
             nome: "Secador",
@@ -134,17 +189,6 @@ async function main() {
             voltagem: 220,
             corrente: 9.1,
             comodoId: banheiro.id
-        }
-    });
-
-    const airfryer = await prisma.dispositivo.create({
-        data: {
-            nome: "Air Fryer",
-            potencia: 1500,
-            tempoUso: 2,
-            voltagem: 220,
-            corrente: 6.82,
-            comodoId: cozinha.id
         }
     });
 
@@ -159,6 +203,50 @@ async function main() {
         }
     });
 
+    const barbeador = await prisma.dispositivo.create({
+        data: {
+            nome: "Máquina de Barbear Elétrica",
+            potencia: 15,
+            tempoUso: 0.2,
+            voltagem: 110,
+            corrente: 0.14,
+            comodoId: banheiro.id
+        }
+    });
+
+    const luminariaEspelho = await prisma.dispositivo.create({
+        data: {
+            nome: "Luminária de Espelho",
+            potencia: 25,
+            tempoUso: 1,
+            voltagem: 110,
+            corrente: 0.23,
+            comodoId: banheiro.id
+        }
+    });
+
+    const ventilador = await prisma.dispositivo.create({
+        data: {
+            nome: "Ventilador",
+            potencia: 80,
+            tempoUso: 6,
+            voltagem: 110,
+            corrente: 0.73,
+            comodoId: quarto.id
+        }
+    });
+
+    const computador = await prisma.dispositivo.create({
+        data: {
+            nome: "Computador",
+            potencia: 300,
+            tempoUso: 8,
+            voltagem: 110,
+            corrente: 2.73,
+            comodoId: quarto.id
+        }
+    });
+
     const aquecedor = await prisma.dispositivo.create({
         data: {
             nome: "Aquecedor",
@@ -166,6 +254,39 @@ async function main() {
             tempoUso: 2,
             voltagem: 220,
             corrente: 6.82,
+            comodoId: quarto.id
+        }
+    });
+
+    const carregadorCelular = await prisma.dispositivo.create({
+        data: {
+            nome: "Carregador de Celular",
+            potencia: 5,
+            tempoUso: 2,
+            voltagem: 110,
+            corrente: 0.045,
+            comodoId: quarto.id
+        }
+    });
+
+    const umidificador = await prisma.dispositivo.create({
+        data: {
+            nome: "Umidificador",
+            potencia: 30,
+            tempoUso: 8,
+            voltagem: 110,
+            corrente: 0.45,
+            comodoId: quarto.id
+        }
+    });
+
+    const led = await prisma.dispositivo.create({
+        data: {
+            nome: "Lâmpada LED",
+            potencia: 10,
+            tempoUso: 5,
+            voltagem: 110,
+            corrente: 0.09,
             comodoId: quarto.id
         }
     });
